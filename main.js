6147110fa34d4cc541dc0ae96ae83ff19b249716
Sunil Kakar <26909769+DrPsyFi@users.noreply.github.com>
2018-03-14 20:07:24 -0700

    quiz complete w/o specific questions

--- main.js
+++ main.js
@@ -1,39 +1,99 @@
+/////////////////////// Variables /////////////////////////////////////////////
 
 const screenA = document.querySelector('.screenA')
-
-const start = document.querySelector('#start')
-  document.addEventListener('click', (event) => {
-
-      start.classList.toggle('d-none')
-      startQuiz(start)
-    })
-
 const ScreenB = document.querySelector('.screenB')
-const quesContain= document.querySelector('#question')
-const ansAContain= document.querySelector('answerA')
-const ansBContain= document.querySelector('answerB')
-const ansCContain= document.querySelector('answerC')
-const ansDContain= document.querySelector('answerD')
+const answerSelector = document.querySelector('.button_box')
+    answerSelector.addEventListener('click', updateScore)
 
-function startQuiz(button) {
-console.log(
-let question = `1. ${quiz[0].question}`
-let answerA = `A.  ${quiz[0].options[0]}`
-let answerB = `B.  ${quiz[0].options[1]}`
-let answerC = `C.  ${quiz[0].options[2]}`
-let answerD = `D.  ${quiz[0].options[3]}`)
-  // renderQuiz(question, answerA, answerB, answerC, answerD)
+let questionIndex = 0
+let total = 0
+//let percentage = total/10 * 100
+
+
+//
+// const start = document.querySelector('#start')
+//   document.addEventListener('click', (event) => {
+//
+//       start.classList.toggle('d-none')
+//       startQuiz(start)
+//     })
+
+///////////////////////////////////////////////////////////////////////////////
+///                           Update Score                                 ////
+///////////////////////////////////////////////////////////////////////////////
+
+function updateScore(e) {
+  let selectedAnswer = e.target.innerHTML
+  let selectedId = selectedAnswer.slice(-1)
+  let testId = quiz[questionIndex].answer
+  if (testId === parseInt(selectedId)) {
+    total += 1
+    getQuestion(e)
+  }
+  else {
+    getQuestion(e)
+  }
+  return total
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/////                         Get Next Question                           /////
+///////////////////////////////////////////////////////////////////////////////
+
+function getQuestion(e) {
+    questionIndex+= 1
+  if (questionIndex <10) {
+    renderQuestion(quiz[questionIndex])
+  }
+  else {
+    renderTotal(e)
+  }
 }
 
 
+///////////////////////////////////////////////////////////////////////////////
+/////               Render Quiz and Answer options                       /////
+///////////////////////////////////////////////////////////////////////////////
 
-//
-// function renderQuiz(question, answerA, answerB, answerC, answerD) {
-//
-//   screenA.innerHTML = {
-//   quesContain.innerHTML = question
-//   ansAContain.innerHTML = answerA
-//   ansBContain.innerHTML = answerB
-//   ansCContain.innerHTML = answerC
-//   ansDContain.innerHTML = answerD
-// }
+renderQuestion(quiz[0])
+
+function renderQuestion(q) {
+  const divQContain = document.querySelector('.question')
+  divQContain.innerHTML = ''
+  const quesContain = document.createElement('div')
+  const question = q.question
+
+  quesContain.innerHTML = question
+  divQContain.appendChild(quesContain)
+  renderOptions(q)
+
+}
+
+function renderOptions(q)  {
+  const divOContain = document.querySelector('.options')
+  divOContain.innerHTML = ''
+  const options = document.createElement('ol')
+
+  for(const opt of q.options) {
+    const li = document.createElement('li')
+    li.innerHTML = opt
+    options.appendChild(li)
+  }
+
+  divOContain.appendChild(options)
+}
+
+//////////////////////// Render Score /////////////////////////////////////////
+function renderTotal(e) {
+
+  let percentage = total/10 * 100
+
+
+  const divQContain = document.querySelector('.question')
+  divQContain.innerHTML = ''
+
+  const divOContain = document.querySelector('.options')
+  divOContain.innerHTML = ''
+  divQContain.innerHTML = `Congratulations, you got ${percentage}%.  You are awesome!`
+}
--- quiz_data.js
+++ quiz_data.js
@@ -2,47 +2,47 @@ var quiz = [
   {
     'question': "Blabedee Blah 1?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 2
+    'answer': 3
   },
   {
     'question': "Blabedee Blah 2?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 1
+    'answer': 2
   },
   {
     'question': "Blabedee Blah 3?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 0
+    'answer': 1
   },
   {
     'question': "Blabedee Blah 4?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 3
+    'answer': 4
   },
   {
     'question': "Blabedee Blah 5?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 1
+    'answer': 2
   },
   {
     'question': "Blabedee Blah 6?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 2
+    'answer': 3
   },
   {
     'question': "Blabedee Blah 7?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 3
+    'answer': 4
   },
   {
     'question': "Blabedee Blah 8?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 0
+    'answer': 1
   },
   {
     'question': "Blabedee Blah 9?",
     'options': ['o1', 'o2', 'o3', 'o4'],
-    'answer': 1
+    'answer': 2
   },
   {
     'question': "Blabedee Blah 10?",
